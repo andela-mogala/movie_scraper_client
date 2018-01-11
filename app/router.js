2a9c02f41fff86baa08f38299f71ea77df2613df
Michael Ogala <michael.ogala@andela.com>
2018-01-11 15:48:06 +0100

    Create show route to display a movie's details

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('movies', function() {
-    // this.route('show', '/:id')
+    this.route('show', { path: ':id'} )
   });
 });
 
